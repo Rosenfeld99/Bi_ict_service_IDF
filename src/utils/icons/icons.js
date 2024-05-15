75aac9fb03ebbf54eb707d6b7828b11da661ec3d
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-05-15 15:44:20 +0300

    add desigen for percangatage for table grid

--- src/utils/icons/icons.js
+++ src/utils/icons/icons.js
@@ -1,5 +1,10 @@
 import { CiEdit, CiSearch, CiTrash } from "react-icons/ci";
-import { FaBuilding, FaCriticalRole, FaRegFileExcel, FaUser } from "react-icons/fa";
+import {
+  FaBuilding,
+  FaCriticalRole,
+  FaRegFileExcel,
+  FaUser,
+} from "react-icons/fa";
 import { FaGears, FaIdCard } from "react-icons/fa6";
 import { GoDatabase, GoNumber } from "react-icons/go";
 import { IoMdClose } from "react-icons/io";
@@ -8,27 +13,29 @@ import { MdLightMode, MdModeNight } from "react-icons/md";
 import { TbMilitaryRank, TbUsers } from "react-icons/tb";
 import { GrPowerReset } from "react-icons/gr";
 import { IoSettings } from "react-icons/io5";
+import { TbPercentage } from "react-icons/tb";
 
 const icons = {
-    Trash: CiTrash,
-    Users: TbUsers,
-    User: FaUser,
-    Data: GoDatabase,
-    Dashboard: LuLayoutDashboard,
-    Excel: FaRegFileExcel,
-    San: MdLightMode,
-    Moon: MdModeNight,
-    Pen: CiEdit,
-    Close: IoMdClose,
-    Search: CiSearch,
-    Card: FaIdCard,
-    Rank: TbMilitaryRank,
-    Numbers: GoNumber,
-    Building: FaBuilding,
-    Role: FaCriticalRole,
-    Gears: FaGears,
-    Reset: GrPowerReset,
-    Settings: IoSettings,
+  Trash: CiTrash,
+  Users: TbUsers,
+  User: FaUser,
+  Data: GoDatabase,
+  Dashboard: LuLayoutDashboard,
+  Excel: FaRegFileExcel,
+  San: MdLightMode,
+  Moon: MdModeNight,
+  Pen: CiEdit,
+  Close: IoMdClose,
+  Search: CiSearch,
+  Card: FaIdCard,
+  Rank: TbMilitaryRank,
+  Numbers: GoNumber,
+  Building: FaBuilding,
+  Role: FaCriticalRole,
+  Gears: FaGears,
+  Reset: GrPowerReset,
+  Settings: IoSettings,
+  Percentage: TbPercentage,
 };
 
 export default icons;
